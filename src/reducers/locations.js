f4e6a1745ccf6e8cb89695f59656974b68f4c526
Alice Freda <alicefreda@gmail.com>
2020-08-10 11:20:03 -0700

    add loading and add locations cases to locations reducer

--- src/reducers/locations.js
+++ src/reducers/locations.js
@@ -1,6 +1,18 @@
-export default (state=[], action) => {
+export default (state={ locations: [], loading: false }, action) => {
     switch (action.type) {
+        case 'LOADING_LOCATIONS':
+            return {
+                ...state,
+                locations: [...state.locations],
+                loading: true
+            }
+        case 'ADD_LOCATIONS':
+            return {
+                ...state,
+                locations: action.locations,
+                loading: false
+            }
         default: 
-        return state
+            return state
     }
 }
