c2c57566fa826e2a69cce5c3af7425dd6ee59165
Alice Freda <alicefreda@gmail.com>
2020-08-10 15:28:39 -0700

    add location actions

---
+++ src/actions/locationActions.js
@@ -0,0 +1,14 @@
+export const fetchLocations = () => {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_LOCATIONS' })
+        fetch('http://localhost:3000/api/v1/locations')
+        .then(response => response.json())
+        .then(responseJSON => {
+            const locations = responseJSON.data.map(location => {
+                return location.attributes
+            });
+            dispatch({ type: 'ADD_LOCATIONS', locations})
+        })
+    }
+}
+
