ae7adbbd5004d353080de07ff8b142467b5a09f6
Alice Freda <alicefreda@gmail.com>
2020-08-10 11:11:53 -0700

    add locations reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import users from './reducers/users.js';
-import regions from '/reducers/regions.js';
+import regions from './reducers/regions.js';
+import locations from './reducers/locations.js'
 
 const reducer = combineReducers({
-  regions
+  regions,
+  locations
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
