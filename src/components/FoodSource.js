58d66bd81178e74abc0fca5f71dd447b1ac5d50b
Alice Freda <alicefreda@gmail.com>
2020-08-12 13:55:30 -0700

    update name of LocationCard component in LocationList component

--- src/components/FoodSource.js
+++ src/components/FoodSource.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FoodSource = ({name, notes}) => {
 
     return (
-        <div className="food-source px-6 py-1">
+        <div className="food-source px-6 py-1 text-gray-900">
             <h4 className="font-semibold">{name}</h4>
             <p>{notes}</p>
         </div>
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -3,15 +3,15 @@ import LocationCard from './LocationCard'
 
 const LocationsList = ({locations}) => {
 
-    const renderLocations = locations.map(location => 
-        <li className="mb-4 shadow-md bg-white" key={location.id}>
+    const renderLocationCards = locations.map(location => 
+        <li className="mb-4 shadow-md bg-white max-w-sm" key={location.id}>
             <LocationCard name={location.name} street={location.street} notes={location.notes} food_sources={location.food_sources}/>
         </li>
     )
     
     return (
         <ul>
-            {renderLocations}
+            {renderLocationCards}
         </ul> 
     )
 }
