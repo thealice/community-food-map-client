c5a932e40687e626a4e4479c4a85663d31d37cc0
Alice Freda <alicefreda@gmail.com>
2020-08-14 15:52:23 -0700

    try to get Location component to register what its location is

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import FoodSource from './FoodSource'
 
-const Location = (props) => {
 
-    const renderFoodSources = props.food_sources.map(f => 
-        <li key={f.key}>
-            <FoodSource name={f.name} notes={f.notes}/>
-        </li>
-    )
+const Location = ({ match, locations, place }) => {
 
+    // const renderFoodSources = place.food_sources.map(f => 
+    //     <li key={f.key}>
+    //         <FoodSource name={f.name} notes={f.notes}/>
+    //     </li>
+    // )
     return (
+
         <div className='location'>
+            {console.log(locations.find(place => (place.id === match.params.id)))}
             
-            <h3>{props.name}</h3>
-            {props.street}
+            {/* <h3>{place.name}</h3>
+            {place.street}
+            {place.city}, {place.state} {place.zipcode} {place.notes}*/}
             
             
-            <ul className="p-6 bg-white"><li>{notes}</li>{renderFoodSources}</ul>
+            {/* <ul className="p-6 bg-white">{renderFoodSources}</ul> */}
         </div>
     )
   }
