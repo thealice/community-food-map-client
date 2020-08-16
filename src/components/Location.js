ffb63a7ea7d147581c4c539aec9dcf3046e9284b
Alice Freda <alicefreda@gmail.com>
2020-08-15 18:24:15 -0700

    basic setup for location layout

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import FoodSource from './FoodSource'
 
+const Location = ({ place }) => {
 
-const Location = ({ match, locations, place}) => {
-    console.log(place && place)
-    const location = locations && locations.find(place => ((place.id === match.params.id)))
-
-    console.log(location)
-    // const renderFoodSources = place.food_sources.map(f => 
-    //     <li key={f.key}>
-    //         <FoodSource name={f.name} notes={f.notes}/>
-    //     </li>
-    // )
+    const renderFoodSources = place.food_sources.map(f => 
+        <li className="py-2" key={f.key}>
+            {f.name}<br/>
+            {f.notes}<br />
+            <button className="btn">Edit Food Source</button>
+        </li>
+    )
+    let address;
+    if(place.street && place.city) {
+        address = `${place.street}, ${place.city}, ${place.state} ${place.zipcode}`
+    }
     return (
 
-        <div className='location'>
-            
-    <h3>Hi this is an individual location {place.name}</h3>
-            
-            
-            
-            {/* <ul className="p-6 bg-white">{renderFoodSources}</ul> */}
+        <div className='location p-8'>  
+            <h1>{place.name}</h1>
+            <p>{address}</p>
+            <p>{place.notes}</p>
+            <h2>Food at this Location:</h2>
+            <ul>{renderFoodSources}</ul>
         </div>
     )
   }
