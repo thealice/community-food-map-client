3562b9e7afd9c39fc47e234ee56ff1a9111bf588
Alice Freda <alicefreda@gmail.com>
2020-08-16 14:44:12 -0700

    small styling change to StyledForm

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,13 +2,16 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 const StyledForm = styled.main.attrs({
-  className: "flex flex-col h-screen justify-center items-center bg-gray-100",
+  className: "flex flex-col h-screen justify-center items-center bg-yellow-100",
 })`
   & {
     form {
-      ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xs`}
+      ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xl`}
     }
     input {
+      ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
+    }
+    textarea {
       ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
     }
     button {
