e5f6ed7c93a8295fa74bc97c214fc47e447f6248
Alice Freda <alicefreda@gmail.com>
2020-08-17 13:10:45 -0700

    add a return in fetchLocations action creator for the dispatch of ADD_LOCATIONS in case that affects the locations returned in the promise

--- src/actions/fetchLocations.js
+++ src/actions/fetchLocations.js
@@ -8,7 +8,7 @@ export const fetchLocations = () => {
             const locations = responseJSON.data.map(location => {
                 return location.attributes
             });
-            dispatch({ type: 'ADD_LOCATIONS', locations})
+            return dispatch({ type: 'ADD_LOCATIONS', locations})
         })
     }
 }
