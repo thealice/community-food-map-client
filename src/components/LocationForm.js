0eadd33b57f8cf530f1ee91a0b8b8361402517c6
Alice Freda <alicefreda@gmail.com>
2020-08-16 17:31:56 -0700

    change label to use htmlFor on Location Form

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -8,36 +8,35 @@ class LocationForm extends Component {
             <div className="location-input p-8">
                 
                 <StyledForm>
-                    <h1>Add a new Location</h1>
                     <form>
-                        <label for="name" className="hidden" >Name:</label>
+                        <label htmlFor="name" className="hidden" >Name:</label>
                         <input type="text" placeholder="Name" name="name" value=""/>
 
-                        <label for="notes" className="hidden" >Notes:</label>
+                        <label htmlFor="notes" className="hidden" >Notes:</label>
                         <textarea placeholder="Notes...for example: 'Fruit accessible from sidewalk is free to take, please do not hop fence.'" name="notes"/>
                         
-                        <label for="street" className="hidden">Street Address</label>
+                        <label htmlFor="street" className="hidden">Street Address</label>
                         <input type="text" placeholder="Street" name="street" value=""/>
 
-                        <label for="city" className="hidden">City</label>
+                        <label htmlFor="city" className="hidden">City</label>
                         <input type="text" placeholder="City" name="city" value=""/>
 
-                        <label for="state" className="hidden">State</label>
+                        <label htmlFor="state" className="hidden">State</label>
                         <input type="text" placeholder="State" name="state" value=""/>
 
-                        <label for="zipcode" className="hidden">Zipcode</label>
+                        <label htmlFor="zipcode" className="hidden">Zipcode</label>
                         <input type="text" placeholder="Zipcode" name="zipcode" value=""/>
 
-                        <label for="latitude" className="hidden">Latitude:</label>
+                        <label htmlFor="latitude" className="hidden">Latitude:</label>
                         <input type="number" placeholder="Latitude (required)" name="lat" value="" required />
                         
-                        <label for="longitude" className="hidden">Longitude:</label>
+                        <label htmlFor="longitude" className="hidden">Longitude:</label>
                         <input type="number" placeholder="Longitude (required)" name="lng" value="" required /> 
                         
-                        <label for="zoom" className="hidden">Zoom:</label>
+                        <label htmlFor="zoom" className="hidden">Zoom:</label>
                         <input type="number" className="hidden" placeholder="Zoom" name="zoom" value="17"/>
 
-                        <button type="submit" classname="button">Submit</button>
+                        <button type="submit" className="button">Submit</button>
                     </form>
                 </StyledForm>
             </div>
