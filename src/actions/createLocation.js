aa7a5631299ad3d85ee52eb6d71e65be4de86f21
Alice Freda <alicefreda@gmail.com>
2020-08-17 17:26:18 -0700

    add add_location to createLocation action creator, and add async post fetch with sanitized form data

--- src/actions/createLocation.js
+++ src/actions/createLocation.js
@@ -1,9 +1,43 @@
 export const createLocation = (locationData) => {
-    return {
-      type: "CREATE_LOCATION",
-      locationData
-    }
+  
+  const sendableLocationData = {
+    name: locationData.name,
+    notes: locationData.notes,
+    street: locationData.street,
+    city: locationData.city,
+    state: locationData.state,
+    zipcode: locationData.zipcode,
+    zoom: parseInt(locationData.zoom),
+    lat: parseFloat(locationData.lat),
+    lng: parseFloat(locationData.lng),
+    user_id: parseInt(locationData.userId),
+    region_id: parseInt(locationData.regionId)
+  }
+
+  return (dispatch) => {
+    return fetch('http://localhost:3000/api/v1/locations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(sendableLocationData)
+    })
+    .then(response => response.json())
+    .catch(console.log)
+    .then(responseJSON => {
+      console.log(responseJSON)
+
+
+      // return dispatch({ type: 'ADD_LOCATION', locations})
+    })
+  }
 }
 
+export const addLocation = location => {
+  return {
+    type: 'ADD_LOCATION',
+    location
+  }
+}
 
 
