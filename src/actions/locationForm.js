31569766f30d06bb232c6b1ee87504aecb97d58a
Alice Freda <alicefreda@gmail.com>
2020-08-17 17:28:30 -0700

    rmeove console.log

--- src/actions/locationForm.js
+++ src/actions/locationForm.js
@@ -1,6 +1,5 @@
 export const updateLocationForm = (name, value) => {
     const formData = { name, value}
-    console.log(formData)
     return {
         type: 'UPDATE_LOCATION_FORM', 
         formData
