ee7184903b1ed52a99f0393e926385e229192d79
Alice Freda <alicefreda@gmail.com>
2020-08-19 18:50:04 -0700

    begin action creator for creating a new Food Source

---
+++ src/actions/createFoodSource.js
@@ -0,0 +1,34 @@
+import { resetFoodSourceForm } from './foodSourceForm'
+
+export const createFoodSource = (foodData, history) => {
+  
+    const sendableFoodData = {
+      name: foodData.name,
+      notes: foodData.notes,
+      location_id: parseInt(foodData.locationId),
+      user_id: parseInt(foodData.userId),
+      available: foodData.available
+    }
+
+    return (dispatch) => {
+        return fetch('http://localhost:3000/api/v1/food_sources', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(sendableFoodData)
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+          if (responseJSON.error) {
+            alert(responseJSON.error)
+          } else {
+            // reset foodSource form values
+            dispatch(resetFoodSourceForm())
+            // reroute to the location page
+            history.push(`/locations/${responseJSON.data.attributes.location_id}`)
+          }
+        })
+      }
+
+}
