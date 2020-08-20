3664929580dfaf48c45e69646b9453b2d50cbbe9
Alice Freda <alicefreda@gmail.com>
2020-08-19 18:45:22 -0700

    add foodSourceForm reducer to store

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import locations from './reducers/locations.js'
 import locationForm from './reducers/locationForm.js'
+import foodSourceForm from './reducers/foodSourceForm.js'
 
 const reducer = combineReducers({
   locations,
-  locationForm
+  locationForm,
+  foodSourceForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
