81a55428dea047a628f36d862f75c6b0e23e68da
Alice Freda <alicefreda@gmail.com>
2020-08-19 18:48:27 -0700

    update return value of reducer and add case for RENDER_FOODSOURCE_FORM

--- src/reducers/foodSourceForm.js
+++ src/reducers/foodSourceForm.js
@@ -1,20 +1,33 @@
 const initialState = {
-    name: "",
-    notes: "",
-    userId: 1,
-    locationId: 0
+    formData: {
+        name: "",
+        notes: "",
+        userId: 1,
+        locationId: 0,
+        available: true
+    },
+    renderForm: false
+    
 }
 
 export default (state=initialState, action) => {
-    // console.log(action)
+    console.log(action)
     switch (action.type) {
     case 'UPDATE_FOODSOURCE_FORM':
         return {
             ...state,
-            [action.formData.name]: action.formData.value
+            formData: {[action.formData.name]: action.formData.value}
         }
     case 'RESET_FOODSOURCE_FORM':
-        return initialState
+        return {
+            ...initialState,
+            renderForm: action.renderForm
+        }
+    case 'RENDER_FOODSOURCE_FORM':
+        return {
+            ...initialState,
+            renderForm: true
+        }
     default:
         return state
     }
