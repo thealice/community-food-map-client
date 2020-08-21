10510ba91444ccc3c6c2c92b40c60272dff7687b
Alice Freda <alicefreda@gmail.com>
2020-08-20 17:41:39 -0700

    remove console log

--- src/actions/locationForm.js
+++ src/actions/locationForm.js
@@ -1,5 +1,6 @@
 export const updateLocationForm = (name, value) => {
-    const formData = { name, value}
+    const formData = { name, value }
+
     return {
         type: 'UPDATE_LOCATION_FORM', 
         formData
