65cf97853e935f654cb35d42bf87f9b8088f0ecb
Alice Freda <alicefreda@gmail.com>
2020-08-23 16:13:58 -0700

    Plot markers for each location and Display Location name in InfoWindow when marker is clicked

--- src/components/MapWithMarkers.js
+++ src/components/MapWithMarkers.js
@@ -1,10 +1,39 @@
 import React from "react"
 import { compose } from "recompose"
-import { withScriptjs, withGoogleMap } from "react-google-maps"
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps"
 
 export const MapWithMarkers = compose(withScriptjs, withGoogleMap)(props => {
 
   return (
-    "Hi, I'm a Google Map"
+
+    <GoogleMap defaultZoom={11} defaultCenter={{ lat: 37.804838, lng: -122.341980 }} >
+  
+      {props.markers.map(marker => {
+        const onClick = props.onClick.bind(this, marker)
+        return (
+          <Marker
+            key={marker.id}
+            onClick={onClick}
+            position={{ lat: parseFloat(marker.lat), lng: parseFloat(marker.lng)}}
+          >
+            {props.selectedMarker === marker &&
+              <InfoWindow>
+                <div>
+                  {marker.name}
+                </div>
+              </InfoWindow>
+            }
+          </Marker>
+        )
+      })}
+
+    </GoogleMap>
+    
   )
 })
