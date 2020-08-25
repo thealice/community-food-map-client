6fae0ced079528571f8b1f7bec2c170ff1cb9468
Alice Freda <alicefreda@gmail.com>
2020-08-24 22:57:07 -0700

    begin to change navbar so links are in hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,29 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <ul className="flex border-b uppercase">
-      <li className="-mb-px mr-1">
-        <NavLink className="bg-transparent inline-block border-l border-t border-r rounded-t py-2 px-4 hover:text-purple-400 font-semibold" to="/">Community Food Map</NavLink>
-      </li>
-      <li className="mr-1">
-        <NavLink className="bg-transparent active:border-l active:border-t active:border-r inline-block py-2 px-4 hover:text-purple-400 font-semibold" to="/about">About</NavLink>
-      </li>
-      <li className="mr-1">
-        <NavLink className="bg-transparent active:border-l active:border-t active:border-r inline-block py-2 px-4 hover:text-purple-400 font-semibold" to="/locations/new">Share some food</NavLink>
-      </li>
-    </ul>
+    <header>
+      <div className="flex items-center justify-between px-4 py-2">
+        <div>
+          <NavLink className="bg-transparent inline-block py-2 px-4 hover:text-purple-400 font-semibold uppercase" to="/">Community Food Map</NavLink>
+        </div>
+        <div>
+          <button type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {/* <div>
+        <NavLink className="bg-transparent block py-2 px-4 hover:text-purple-400 font-semibold" to="/about">About</NavLink>
+        <NavLink className="bg-transparent block py-2 px-4 hover:text-purple-400 font-semibold" to="/locations/new">Share some food</NavLink>
+      </div> */}
+
+  </header>
+
   )
 }
 
 export default NavBar;
+
