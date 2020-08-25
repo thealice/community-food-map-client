951342b4251621b4f1b9f5a953b223baea6985e8
Alice Freda <alicefreda@gmail.com>
2020-08-25 16:07:12 -0700

    try locations list as grid of 4

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -11,7 +11,7 @@ const LocationsList = ({ locations }) => {
     )
     
     return (
-        <div className="locations-list grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mx-4">
+        <div className="locations-list grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-4">
             
             {renderLocationCards}
 
